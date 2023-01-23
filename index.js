0c2e59d614d31d764f3baef7bf1516f2ed85a2a9
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-01-23 19:06:32 +0200

    practice

--- index.js
+++ index.js
@@ -14,8 +14,21 @@ function addNums(n) {
 
   console.timeLog("Timer 1");  // Timer 1: 0.104ms
 
-  console.timeEnd("Timer 1"); // Timer 1: 3.292ms*/
+  console.timeEnd("Timer 1"); // Timer 1: 3.292ms
 
   console.time("addNums");
   addNums(1000000);
-  console.timeEnd("addNums");// addNums: 16.211ms
+  console.timeEnd("addNums");// addNums: 16.211ms */
+
+  startTime = Date.now();
+  addNums(1000000);
+  endTime = Date.now();
+
+  console.log(startTime); // 1674493371410
+  console.log(endTime); //1674493412581
+
+  /*Taking the difference between the end and start times 
+  will tell you how long the code took to run.*/
+
+  console.log(`Runtime: ${endTime - startTime}ms`); 
+  //runtime: 24ms
